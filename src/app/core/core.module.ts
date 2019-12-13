1761a517f4e194abc978cc9ca8934a385cb3a2a7
Alejandro Garrido <alejandro.garrido@boream.com>
2019-12-13 22:26:07 +0100

    Add header-component in CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { MatButtonModule, MatToolbarModule } from '@angular/material';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+    if (parent) {
+      throw new Error('CoreModule is already imported');
+    }
+  }
+}
