40c5fadcaba43deb039869fab3016c45ac0500ea
Alejandro Garrido <alejandro.garrido@boream.com>
2019-12-15 16:09:58 +0100

    Add dependency test

--- src/app/books/components/book-filter/book-filter.component.spec.ts
+++ src/app/books/components/book-filter/book-filter.component.spec.ts
@@ -1,30 +1,48 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 
 import { BookFilterComponent } from './book-filter.component';
-import { AngularMaterialModule } from '../../../shared/angular-material.module';
+import { BooksService } from '../../services/books.service';
 import { ReactiveFormsModule } from '@angular/forms';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import 'zone.js/dist/zone-patch-rxjs-fake-async';
 
 describe('BookFilterComponent', () => {
   let component: BookFilterComponent;
   let fixture: ComponentFixture<BookFilterComponent>;
 
-  beforeEach(async(() => {
+  let booksServiceStub: Partial<BooksService>;
+
+  beforeEach(fakeAsync(() => {
+    booksServiceStub = {
+      titleChange$: new BehaviorSubject<string>(''),
+      authorChange$: new BehaviorSubject<string>(''),
+      categoryChange$: new BehaviorSubject<string>('')
+    };
     TestBed.configureTestingModule({
       declarations: [ BookFilterComponent ],
-      imports: [AngularMaterialModule, ReactiveFormsModule, NoopAnimationsModule, HttpClientModule]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
+      imports: [ReactiveFormsModule],
+      providers: [ {provide: BooksService, useValue: booksServiceStub } ],
+      schemas: [ NO_ERRORS_SCHEMA ]
+    });
     fixture = TestBed.createComponent(BookFilterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+    component.ngOnInit();
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should pass title input to booksService', () => {
+    const booksService = fixture.debugElement.injector.get(BooksService);
+    component.bookFilter.get('title').setValue('a');
+    fixture.detectChanges();
+    // tick(); --> No hace falta porque la ejecución del subscribe es síncrona
+    // fixture.detectChanges();
+    booksService.titleChange$.subscribe(v => {
+      expect(v).toBe('a');
+    });
   });
 });
