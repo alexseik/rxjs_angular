0393ab23dc35000254878f7ca1ed0f4daa7b605c
Alejandro Garrido <alejandro.garrido@boream.com>
2019-12-16 22:15:38 +0100

    Fix book service test

--- src/app/books/models/author.ts
+++ src/app/books/models/author.ts
@@ -1,5 +1,5 @@
 export interface Author {
-  id: string;
+  id?: string;
   name: string;
-  categories: string[];
+  categories?: string[];
 }
--- src/app/books/services/books.service.spec.ts
+++ src/app/books/services/books.service.spec.ts
@@ -1,12 +1,37 @@
 import { fakeAsync, TestBed, tick, ComponentFixture } from '@angular/core/testing';
 import 'zone.js/dist/zone-patch-rxjs-fake-async';
-
+import { cold, getTestScheduler } from 'jasmine-marbles';
 import { BooksService } from './books.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('BooksService', () => {
   let httpMock: HttpTestingController;
   let service: BooksService;
+
+  let postSpy;
+
+  const dummyBooks = [
+    {
+      isbn: '12341234',
+      title: 'libro 1',
+      language: 'spanish',
+      authors: ['1', '2'],
+      categories: ['sfy']
+    }
+  ];
+
+  const dummyAuthors = [
+    {
+      id: '1',
+      name: 'author 1',
+      categories: ['kids']
+    },
+    {
+      id: '2',
+      name: 'author 2',
+      categories: ['kids']
+    },
+  ];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -24,28 +49,6 @@ describe('BooksService', () => {
   });
 
   it('should return books with authors', fakeAsync(() => {
-    const dummyBooks = [
-      {
-        isbn: '12341234',
-        title: 'libro 1',
-        language: 'spanish',
-        authors: ['1', '2'],
-        categories: ['sfy']
-      }
-    ];
-
-    const dummyAuthors = [
-      {
-        id: '1',
-        name: 'author 1',
-        categories: ['kids']
-      },
-      {
-        id: '2',
-        name: 'author 2',
-        categories: ['kids']
-      },
-    ];
 
     service.filteredBooks$.subscribe((books) => {
       expect(books).toBeDefined();
@@ -64,6 +67,64 @@ describe('BooksService', () => {
     expect(reqs.length).toBe(2);
     reqs[1].flush(dummyAuthors); // se necesita porque se hace una peticion a authors
     reqs[0].flush(dummyBooks);
+    tick();
+  }));
+
+  it('should save books and new authors', fakeAsync(() => {
+    service.filteredBooks$.subscribe((books) => {
+      expect(books).toBeDefined();
+      if (books.length > 0) {
+        expect(books.length).toBe(1);
+        const book = books[0];
+        expect(book.authors.length).toBe(2);
+        const firstAuthor = book.authors[0];
+        expect(firstAuthor.name).toBe('author 1');
+      }
+    });
+
+    const book = {
+      id: 'otro',
+      isbn: 'otro isbn',
+      title: 'otro title',
+      authors: [
+        {
+          id: '1',
+          name: 'aurelio',
+        }, {
+          name: 'otro autor'
+        }
+      ],
+      categories: []
+    };
+    service.saveBook(book);
+    // debugger
+    const postAuthor = httpMock.match({ method: 'POST' });
+    let putBook = httpMock.match({ method: 'PUT' });
+    expect(postAuthor.length).toBe(1);
+    expect(putBook.length).toBe(0);
+    const dummyAuthor = {
+      id: 'otro',
+      name: 'otro autor',
+      categories: []
+    };
+    postAuthor[0].flush(dummyAuthor);
+    tick();
+    putBook = httpMock.match({ method: 'PUT' });
+    expect(putBook.length).toBe(1);
+    const dummyBook = {
+      isbn: 'otro isbn',
+      title: 'otro title',
+      authors: ['1', 'otro'],
+      categories: []
+    };
+    putBook[0].flush(dummyBook);
+    // debugger
+    tick();
+    const getRequests = httpMock.match({ method: 'GET' });
+    expect(getRequests.length).toBe(2);
+    // debugger
+    getRequests[0].flush([dummyBook]);
+    getRequests[1].flush([...dummyAuthors, dummyAuthor]);
     tick();
   }));
 });
