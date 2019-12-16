dac94171e16193a5587bbb7b394c644d0084737e
Alejandro Garrido <alejandro.garrido@boream.com>
2019-12-16 13:22:28 +0100

    Fix FilmsComponent test

--- src/app/films/components/films/films.component.spec.ts
+++ src/app/films/components/films/films.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FilmsComponent } from './films.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('FilmsComponent', () => {
   let component: FilmsComponent;
@@ -8,7 +9,8 @@ describe('FilmsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FilmsComponent ]
+      declarations: [ FilmsComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
